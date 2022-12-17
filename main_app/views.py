34ff68665c0113475a0f3b572ebfa09590182414
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-16 17:04:01 -0800

    details

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Finch
+# class Finch: 
+#     def __init__(self, name, species, description, age):
+#         self.name = name
+#         self.species = species
+#         self.description = description
+#         self.age = age
 
-class Finch: 
-    def __init__(self, name, species, description, age):
-        self.name = name
-        self.species = species
-        self.description = description
-        self.age = age
-
-finches = [
-    Finch('Mary', 'Black Rosy Finch', 'This nearly black finch has a gray cap and pink highlights on the wings and belly.', 2 ),
-    Finch('Bob', 'Brown-capped Rosy-Finches', 'With its rosy pink belly and brown upperparts, the Brown-capped Rosy-Finch looks like raspberry ice cream smothered in chocolate', 1 ),
-    Finch('Sal', 'Darwin Finches', "Darwin's finches are a group of about 18 species of passerine birds. They are well known for their remarkable diversity in beak form and function.", 10 ),
-]
+# finches = [
+#     Finch('Mary', 'Black Rosy Finch', 'This nearly black finch has a gray cap and pink highlights on the wings and belly.', 2 ),
+#     Finch('Bob', 'Brown-capped Rosy-Finches', 'With its rosy pink belly and brown upperparts, the Brown-capped Rosy-Finch looks like raspberry ice cream smothered in chocolate', 1 ),
+#     Finch('Sal', 'Darwin Finches', "Darwin's finches are a group of about 18 species of passerine birds. They are well known for their remarkable diversity in beak form and function.", 10 ),
+# ]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>');
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render (request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
