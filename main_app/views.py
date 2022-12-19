896c9a045e1f8550c5d115398ce5ad0685cb0aad
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-19 11:16:49 -0800

    Full CRUD

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+
+
 # class Finch: 
 #     def __init__(self, name, species, description, age):
 #         self.name = name
@@ -20,9 +24,27 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def finches_index(request):
-    finches = Finch.objects.all()
-    return render (request, 'finches/index.html', { 'finches': finches })
+# def finches_index(request):
+#     finches = Finch.objects.all()
+#     return render (request, 'finches/index.html', { 'finches': finches })
+
+class FinchList(ListView):
+    model = Finch
+    template_name = 'finches/index.html'
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['species', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
