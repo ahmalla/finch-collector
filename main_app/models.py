6aa8e2d58123d00a2b86f92baf69df6bdf1a2979
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-20 09:22:14 -0800

    adding m:m between finch and trinket

--- main_app/models.py
+++ main_app/models.py
@@ -29,6 +29,7 @@ class Finch(models.Model):
     species = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    trinkets = models.ManyToManyField(Trinket)
 
     def __str__(self):
         return f'{self.name}' ({self.id})
