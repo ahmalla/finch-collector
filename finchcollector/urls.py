14a41ef5e3ecf86894a326691c67f93959e36b03
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-20 14:07:03 -0800

    added registration folder and html file for login page, also added login redirect to the bottom of the settings.py

--- finchcollector/urls.py
+++ finchcollector/urls.py
@@ -21,3 +21,14 @@ urlpatterns = [
     path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# accounts path automatically adds the following code into the app:
+
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
