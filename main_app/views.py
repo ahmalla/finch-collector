f24fbe7465a9d1078694bb4bed3498571cbf3636
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-20 14:41:58 -0800

    add imports for signup in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Finch, Trinket
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .forms import FeedingForm
 
 
